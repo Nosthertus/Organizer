fc7535627662a328069536817462d8a77ac7cbce
Nosthertus <darkfreak17@hotmail.com>
2015-06-17 02:53:07 -0430

    Server:     +Added Task controller in Api module(All, View, Create)     +Added Api functions to Task model
        *Modified apiModule to set header for application/json

--- protected/modules/api/ApiModule.php
+++ protected/modules/api/ApiModule.php
@@ -20,6 +20,9 @@ class ApiModule extends CWebModule
 		{
 			// this method is called before any module controller action is performed
 			// you may place customized code here
+
+			header('Content-Type: application/json');
+
 			return true;
 		}
 		else
